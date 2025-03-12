a197502637e269a511c70cf3ef8be91005e33dbf
TallesOlvr7 <tallesgabrieldeoliveirarodrig.com>
2025-03-12 18:04:13 -0300

    feat:making routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\API\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get("/products",[ProductController::class, 'index']);
+Route::post("/products",[ProductController::class, 'store']);
+
