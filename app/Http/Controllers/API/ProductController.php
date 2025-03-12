a7573ef0a13f71bc18b7e84956f01298756840a8
TallesOlvr7 <tallesgabrieldeoliveirarodrig.com>
2025-03-12 18:04:02 -0300

    fix:fixing controllers

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateProductRequest;
+use App\Models\Product;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public  function store(CreateProductRequest $request):JsonResponse
+    {
+        $productRequest = $request->validated();
+        $product = Product::create($productRequest);
+
+        return response()->json($product);
+    }
+
+    public function all():JsonResponse
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
 }
